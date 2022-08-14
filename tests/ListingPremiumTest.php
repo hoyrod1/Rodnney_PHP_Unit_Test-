9fec2adb857bb0ef379898d8b8392ce5411e774f
Rodney St. Cloud <hoyrod1@aol.com>
2022-08-14 13:10:26 -0400

    Completed Testing for ListingPremium Class
    All testing completed and passed for  getStatus method and  getDescription method in the LightPremium class

--- tests/ListingPremiumTest.php
+++ tests/ListingPremiumTest.php
@@ -4,30 +4,51 @@ use PHPUnit\Framework\TestCase;
 
 class ListingPremiumTest extends TestCase
 {
+    protected $data = array();
+    protected $ListingPremium;
+    protected $results;
+
     /** @test */
     public function getStatusPremium()
     {
         $expectedResults = 'premium';
+    
+        $this->data = ['id' => "3", 
+                       "title" => "First Test", 
+                       'website' => 'http://www.teamtreehouse.com',
+                       'email' => 'teamtreehouse@test.org',
+                       'twitter' => 'twitter.com/teamtreehouse',
+                       'description' => 'This test is specifically for the premuim members. This section allows premium members to access premium services'
+                       ];
 
-        $listingPremium = new ListingPremium();
+        $this->ListingPremium = new ListingPremium($this->data);
         
-        $results = $listingPremium->getStatus();
+        $this->results = $this->ListingPremium->getStatus();
 
-        $this->assertEquals($expectedResults, $results, "The getStatus method does not return basic");
+        $this->assertEquals($expectedResults, $this->results, "The getStatus method does not return premium");
     }
  
     /** @test */
     public function getDescriptionReturnsExpectedResults()
     {
-        $expectedResults = 'premium';
+        // $expectedResults = 'This test is specifically for the premuim members. This section allows premium members to access premium services';
+    
+        $this->data = ['id' => "3", 
+                       "title" => "First Test", 
+                       'website' => 'http://www.teamtreehouse.com',
+                       'email' => 'teamtreehouse@test.org',
+                       'twitter' => 'twitter.com/teamtreehouse',
+                       'description' => ''
+                       ];
 
-        $listingPremium = new ListingPremium();
+        $expectedResults = $this->data['description'];               
+
+        $this->listingPremium = new ListingPremium($this->data);
         
-        $results = $listingPremium->getDescription();
+        $results = $this->listingPremium->getDescription();
 
-        $this->assertEquals($expectedResults, $results, "The getStatus method does not return basic");
+        $this->assertEquals($expectedResults, $results, "The getDescription method does not return expected description");
     }  
     
     
-
 }
