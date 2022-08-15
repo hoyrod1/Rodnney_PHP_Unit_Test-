9fec10b1e7f3c28cd7ac378433885f7211f9b812
Rodney St. Cloud <hoyrod1@aol.com>
2022-08-14 22:22:37 -0400

    All Test Completed for ListingFeatured Class
    All Test Completed and passed for
    ListingFeatured Class.

--- tests/ListingFeaturedTest.php
+++ tests/ListingFeaturedTest.php
@@ -4,28 +4,50 @@ use PHPUnit\Framework\TestCase;
 
 class ListingFeaturedTest extends TestCase
 {
+    protected $data = array();
+    protected $listingFeatured;
+    protected $results;
+
     /** @test */
     public function getStatusFeatured()
     {
         $expectedResults = 'featured';
+    
+        $this->data = ['id' => "3", 
+                       "title" => "First Test", 
+                       'website' => 'http://www.teamtreehouse.com',
+                       'email' => 'teamtreehouse@test.org',
+                       'twitter' => 'twitter.com/teamtreehouse',
+                       'description' => 'This test is specifically for the premuim members. This section allows premium members to access premium services',
+                       'coc' => 'One for All Events proudly stands by our long tradition of providing safe, secure, and productive environments at all of our events. We want to provide a welcoming experience to all attendees, old and new, and enjoy the great hospitality that the community has to offer together.'                       
+                        ];
 
-        $listingFeatured = new ListingFeatured();
+        $this->listingFeatured = new ListingFeatured($this->data);
         
-        $results = $listingFeatured->getStatus();
+        $this->results = $this->listingFeatured->getStatus();
 
-        $this->assertEquals($expectedResults, $results, "The getStatus method does not return basic");
+        $this->assertEquals($expectedResults, $this->results, "The getStatus method does not return featured status");
     }
  
     /** @test */
     public function geCocReturnsExpectedResults()
     {
-        $expectedResults = 'premium';
-
-        $listingFeatured = new ListingFeatured();
+        $this->data = ['id' => "3", 
+                       "title" => "First Test", 
+                       'website' => 'http://www.teamtreehouse.com',
+                       'email' => 'teamtreehouse@test.org',
+                       'twitter' => 'twitter.com/teamtreehouse',
+                       'description' => 'This test is specifically for the premuim members. This section allows premium members to access premium services',
+                       'coc' => 'One for All Events proudly stands by our long tradition of providing safe, secure, and productive environments at all of our events. We want to provide a welcoming experience to all attendees, old and new, and enjoy the great hospitality that the community has to offer together.'                       
+                        ];
         
-        $results = $listingFeatured->getCoc();
+        $expectedResults = $this->data['coc'];                
 
-        $this->assertEquals($expectedResults, $results, "The getStatus method does not return basic");
+        $this->listingFeatured = new ListingFeatured($this->data);
+        
+        $this->results = $this->listingFeatured->getCoc();
+
+        $this->assertEquals($expectedResults, $this->results, "The getCoc method does not return expected results");
     }  
     
     
